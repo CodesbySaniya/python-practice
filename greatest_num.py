f75e157b772fa0bca6ab9412fda138cbe9e85237
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-03-06 21:43:44 +0530

    Create greatest_num.py

---
+++ greatest_num.py
@@ -0,0 +1,10 @@
+#greatest of 3 number
+num1=int(input("enter 1st no."))
+num2=int(input("enter 2nd no."))
+num3=int(input("enter 3rd no."))
+if( num1>= num2 and num1>=num3):
+  print("num1 is greater")
+elif( num2>= num1 and num2>=num3):
+  print("num2 is greater")
+else:
+  print("num3 is greater")
