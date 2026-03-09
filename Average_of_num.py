56e31a5c79150efbb97053255e938faa9de613de
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-03-09 23:51:29 +0530

    Create Average_of_num.py

---
+++ Average_of_num.py
@@ -0,0 +1,8 @@
+# Problem: HackerRank Lists
+# Language: Python
+# Author: Saniya
+#input 2 floating point number and print their average
+num1=float(input("enter 1st number:"))
+num2=float(input("enter 2nd number:"))
+avg=(num1+num2)/2
+print(avg)
