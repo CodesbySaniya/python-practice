66189e6ac2ec9542284dfcd39f0f38d8d30c8ceb
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-10 23:36:13 +0530

    Update for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -1,4 +1,30 @@
-#using for loop
-i=[1,2,3,4,5]
- for values in i:
-   print(values)
+
+for x in range(1,11):
+  print(x)
+print("HAPPY BIRTHDAY!")
+
+
+#to reverse
+for x in reversed(range(1,11)):
+ print(x)
+print("HAPPY BIRTHDAY!")
+
+#to take a step
+for i in range(1,21,):
+  print(i)
+
+pin_num="5678-0000-4500-8953"
+for i in pin_num:
+  print(i)
+
+for x in range(1,21):
+  if x == 7:
+    continue#use to skip value only
+  else:
+    print(x)
+
+for x in range(1,21):
+  if x == 7:
+    break#use to terminate whole prgoram
+  else:
+    print(x)
