f3d76ec5689d1ae0a65e36e3c531017c2e7e1e49
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-11 22:18:56 +0530

    Create list_methods.py

---
+++ list_methods.py
@@ -0,0 +1,20 @@
+#collection-single variable used to store multiple value
+
+marks=[34,65,34,87,99,34,65,88]
+print(marks)
+marks.append(54)#ends at last
+print(marks)
+marks.insert(0,76)#insert at index position we give
+print(marks)
+marks.pop(1)#pop with index number
+print(marks)
+marks.copy()
+print(marks)
+marks.sort()
+print(marks)
+marks.reverse()
+print(marks)
+marks.remove(99)#remove with number
+print(marks)
+marks.clear()#clear lists
+print(marks)
