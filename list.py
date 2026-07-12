6a9cb6c8b2c2fa294cda6f9f0f5d154ab6458238
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-12 23:40:37 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,9 +1,31 @@
-#list using loop
+#list(basic to advance)
 
-actor = ["shahrukh","ajay","amitabh","salman","sidharth","varun"]
+numbers = [10 , 20 , 30 , 40, 50]
+print(numbers[0])#indexing
+print(numbers[2])
+print(numbers[-1])
 
-actor[0] = "vicky"
-for x in actor:
-  print(x)
-  print(x.upper())
+numbers[2]=300#updating
+print(numbers)
+
+numbers = [10 , 20 , 30 , 40, 50]
+print(numbers[0:3])#slicing
+print(numbers[-1:-3:-1])#in reverse order
+#list(basic to advance)
+
+numbers = [10 , 20 , 30 , 40, 50]
+print(numbers[0])#indexing
+print(numbers[2])
+print(numbers[-1])
+
+numbers[2]=300#updating
+print(numbers)
+
+numbers = [10 , 20 , 30 , 40, 50]
+print(numbers[0:3])#slicing
+print(numbers[-1:-3:-1])#in reverse
+print(numbers[-2:])#or
+print(numbers[3:])#or
+print(numbers[1:4])
+
 
