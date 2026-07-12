ebae61e0470c7fccd2cda6140c8fc4c2f28e690e
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-12 23:55:29 +0530

    Update list_methods.py

--- list_methods.py
+++ list_methods.py
@@ -18,3 +18,25 @@ marks.remove(99)#remove with number
 print(marks)
 marks.clear()#clear lists
 print(marks)
+
+
+numbers = [10,20,30]
+numbers.append(40)
+numbers.append(50)
+print(numbers)
+
+numbers.insert(1,15)
+numbers.insert(3,25)
+print(numbers)
+
+
+list1 = [1,2]
+list2 = [3,4]
+
+list1.extend(list2)
+print(list1)
+
+list1 = [10,20]
+list2 = [30,40]
+list1.extend(list2)
+print(list1)
