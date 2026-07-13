5227c4bf3e532749b6526287c77b214a39d838dc
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-13 23:51:33 +0530

    Create 2d_list.py

---
+++ 2d_list.py
@@ -0,0 +1,14 @@
+#2D list
+
+fruits =    ["mango","orange","banana","pineapple"]
+vegetable = ["onion","potato","garlic","capsicum","cucumber"]
+drinks =    ["coffe","tea","juice","cold-drinks"]
+
+groceries = [fruits,vegetable,drinks]
+
+print(groceries)
+print(groceries[0])#print fruit list
+print(groceries[1])#print vegetable list
+print(groceries[2])#print drinks list
+
+print(groceries[0][1])#0th row and 1 coloumn returns orange
