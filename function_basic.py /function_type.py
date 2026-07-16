7afdf3e33f36cefe411e936a6dc3406f67761269
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-16 22:37:22 +0530

    Update function_type.py

--- function_basic.py /function_type.py
+++ function_basic.py /function_type.py
@@ -3,3 +3,11 @@ def detail(name , branch="ce" , year=3):
   return  f" your name: {name} \n your branch: {branch}  \n year:  {year} "
 
 print(detail("saniya"))
+
+
+#keyword argument (posotion of argument doesnt matter)
+
+def detail(name , branch, year): #default argument
+  return  f"your name: {name} ,  your branch: {branch}  , year:  {year} "
+
+print(detail(name = "saniya",year = "3rd",branch = "cse"))
