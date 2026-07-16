07300e3cd333c8689a696ec01807418774ad27b0
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-16 23:48:31 +0530

    Update function_type.py

--- function_basic.py /function_type.py
+++ function_basic.py /function_type.py
@@ -11,3 +11,30 @@ def detail(name , branch, year): #default argument
   return  f"your name: {name} ,  your branch: {branch}  , year:  {year} "
 
 print(detail(name = "saniya",year = "3rd",branch = "cse"))
+
+# #arbitary argument stores multiple non key values #we use tuple
+def add(*args):
+  total = 0
+  for arg in args:
+    total +=arg
+  return total
+
+print(add(2,3,5,6))
+
+def name(*args):
+  for arg in args:
+    print(arg , end=" ")
+  
+
+print(name("saniya","neha"))
+
+#keyword arbitary argument type - dictionary and can store multiple kyeywords
+
+def detail(**kwargs):
+  for key,value in kwargs.items():
+    print(f"{key}:{value}")
+
+detail(
+    name="saniya", 
+    branch="cse",
+    year="3rd")
