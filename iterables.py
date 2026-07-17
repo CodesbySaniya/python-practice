cd7327a96fc9702b83b390b168ac417e613e0a3a
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-17 23:45:17 +0530

    Update iterables.py

--- iterables.py
+++ iterables.py
@@ -14,3 +14,19 @@ name = "saniya" #string is also iterable
 
 for name in name:
   print(name)
+
+
+fruits = {"mango","cherry","strawberry"}#set is also iterable
+
+for fruit in fruits:
+  print(fruit)
+
+my_dict = {"A" : 1 ,"B" : 2 , "C" : 3}
+for key in my_dict:
+  print(key)
+
+for value in my_dict.values():
+  print(value)
+
+for item in my_dict.items():
+  print(item)
