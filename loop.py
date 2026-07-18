483d3a16ff9173f1bbf5eab119ca114a16038794
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-18 23:02:36 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -3,3 +3,11 @@ num=1
 while num <= 5:
  print(num)
  num+=1
+
+#print only even number
+list1 = [3, 8, 11, 14, 17,43,52,78,65,41,-75,98,20]
+list2=[]
+for i in list1:
+  if i % 2 == 0:
+    list2.append(i)
+print(list2)
