9722bea19ff88dc2bbd79ca3a366c9610c03007d
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-20 23:30:42 +0530

    Update module.py

--- module.py
+++ module.py
@@ -8,3 +8,27 @@ print(e)
 
 from math import log
 print(log(2))
+
+
+#saniya module
+pi = 3.1459
+
+def square(x):
+  return x**2
+
+def cube(x):
+  return x**3
+
+def circumference(radius):
+  return 2*pi*radius
+
+def area(radius):
+  return pi * radius**2
+
+import saniya
+
+result = saniya.area(5)
+result = saniya.square(5)
+result = saniya.cube(5)
+result = saniya.circumference(5)
+print(result)
